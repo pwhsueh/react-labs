dc0b5cb52e79a68605fff794289ce67512effdc6
bowen_hsueh <pwhsueh@gmail.com>
2025-02-22 17:39:10 +0800

    refactor App component to render CoreConcepts dynamically using map function

--- src/App.jsx
+++ src/App.jsx
@@ -35,21 +35,10 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-          <CoreConcept 
-            {...CORE_CONCEPTS[0]}
-             />
-          <CoreConcept 
-            {...CORE_CONCEPTS[1]}
-             />
-          
-          <CoreConcept 
-            {...CORE_CONCEPTS[2]}
-             />
-          
-          <CoreConcept 
-            {...CORE_CONCEPTS[3]}
-             />
-             </ul>
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} 
+            {...conceptItem}
+             /> )}
+          </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
