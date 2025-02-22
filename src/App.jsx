a75fc125f7a0f2a9e700628c95a1ec49855a7ba9
bowen_hsueh <pwhsueh@gmail.com>
2025-02-22 15:34:55 +0800

    add example content and update App component to display selected topic details

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react';
 import CoreConcept from './components/CoreConcept.jsx';
 import Header from './components/Header/Header.jsx';
 import TabButton from './components/TabButton.jsx';
-import { CORE_CONCEPTS } from './data.js';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 
 function App() {
 
-  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   function handleClick(selectedButton) {
     // console.log(`Switch to tab: ${selectedButton}`);
@@ -41,11 +41,19 @@ function App() {
           <h2>Examples</h2>
           <menu>
             <TabButton onSelect={() => handleClick('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleClick('jSX')}>JSX</TabButton> 
+            <TabButton onSelect={() => handleClick('jsx')}>JSX</TabButton> 
             <TabButton onSelect={() => handleClick('props')}>Props</TabButton> 
             <TabButton onSelect={() => handleClick('state')}>State</TabButton>  
           </menu> 
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
