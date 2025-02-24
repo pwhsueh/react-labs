acff78bd01d448c8b5c5e8f4113426d7e8d1bdbe
bowen_hsueh <pwhsueh@gmail.com>
2025-02-24 14:05:14 +0800

    refactor Tabs component to set default ButtonsContainer prop and update Examples component to remove prop usage

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -31,7 +31,7 @@ export default function Examples() {
 
     return (
         <Section title="Examples" id="examples">
-            <Tabs ButtonsContainer="menu" buttons={
+            <Tabs buttons={
                 <>
                     <TabButton isSelected={selectedTopic == 'components'} onClick={() => handleClick('components')}>Components</TabButton>
                     <TabButton isSelected={selectedTopic == 'jsx'} onClick={() => handleClick('jsx')}>JSX</TabButton> 
